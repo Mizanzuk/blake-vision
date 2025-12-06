d88b0d3f84354d9bf97628250511fdd9514e818d
Blake Vision Dev <ivancoxa@gmail.com>
2025-12-06 17:55:48 -0500

    Fix: Adicionar pb-20 no wrapper externo para respiro visual

--- app/escrita/page.tsx
+++ app/escrita/page.tsx
@@ -1593,7 +1593,7 @@ function EscritaPageContent() {
 
               {/* Conteúdo (só aparece após salvar metadados) */}
               {isMetadataSaved && (
-                <div className="-mt-4">
+                <div className="-mt-4 pb-20">
                   <RichTextEditor
                     value={conteudo}
                     onChange={(value) => setConteudo(value)}
